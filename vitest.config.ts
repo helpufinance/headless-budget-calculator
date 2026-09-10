e003c493a4ded28e05f50209fe1d86f367a4faef
Sándor Gergely Bornemisza <bornemisza@omisai.com>
2026-09-10 13:46:41 +0200

    chore: update Vitest coverage configuration with thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,15 @@ export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
     coverage: {
+      include: ['src/**/*.ts'],
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
     },
   },
 })
